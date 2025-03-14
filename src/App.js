10ab9990588e08b90e217f0dfd5396adb2a1b694
shahrukhnagnur <shahrukhnagnur2535@gmail.com>
2025-03-14 23:29:15 +0530

    Fixed untracked changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
-import { BrowserRouter, Routes, Route, useLocation, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+  Navigate,
+} from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
-import Contact from "./Contact";  
+import Contact from "./Contact";
 import Navbar from "./Navbar";
 import Footer from "./footer";
 import WhatsappIcon from "./whatsappicon";
 import Login from "./auth/login";
 import Register from "./auth/signup";
-import Parts from "./parts";  // Import Parts component
+import Parts from "./parts"; // Import Parts component
 
 const Layout = ({ children }) => {
   const location = useLocation();
-  const hideLayout = location.pathname === "/login" || location.pathname === "/register";
+  const hideLayout =
+    location.pathname === "/login" || location.pathname === "/register";
 
   return (
     <>
@@ -41,70 +48,70 @@ const App = () => {
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route 
-            path="/login" 
+          <Route
+            path="/login"
             element={
               <AuthRedirect>
                 <Login />
               </AuthRedirect>
-            } 
+            }
           />
-          <Route 
-            path="/register" 
+          <Route
+            path="/register"
             element={
               <AuthRedirect>
                 <Register />
               </AuthRedirect>
-            } 
+            }
           />
-          
-          <Route 
-            path="/" 
+
+          <Route
+            path="/"
             element={
               <ProtectedRoute>
                 <Home />
               </ProtectedRoute>
-            } 
+            }
           />
-          <Route 
-            path="/home" 
+          <Route
+            path="/home"
             element={
               <ProtectedRoute>
                 <Home />
               </ProtectedRoute>
-            } 
+            }
           />
-          <Route 
-            path="/about" 
+          <Route
+            path="/about"
             element={
               <ProtectedRoute>
                 <About />
               </ProtectedRoute>
-            } 
+            }
           />
-          <Route 
-            path="/contact" 
+          <Route
+            path="/contact"
             element={
               <ProtectedRoute>
                 <Contact />
               </ProtectedRoute>
-            } 
+            }
           />
-          <Route 
-            path="/whatsappicon" 
+          <Route
+            path="/whatsappicon"
             element={
               <ProtectedRoute>
                 <WhatsappIcon />
               </ProtectedRoute>
-            } 
+            }
           />
-          <Route 
-            path="/parts"  
+          <Route
+            path="/parts"
             element={
               <ProtectedRoute>
                 <Parts />
               </ProtectedRoute>
-            } 
+            }
           />
         </Routes>
       </Layout>
